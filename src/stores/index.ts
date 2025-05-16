d4a13f99123b9c0e73ae7d5daa113ee5b1be1e50
c-denis <den.smertch@gmail.com>
2025-05-16 12:25:19 +0300

    Initial commit - clean project structure

--- src/stores/index.ts
+++
@@ -1,4 +0,0 @@
-export { useAuthStore } from './auth.store'
-export { useUsersStore } from './users.store'
-export { useRequestsStore } from './requests.store'
-export { useNotificationsStore } from './notifications.store'
